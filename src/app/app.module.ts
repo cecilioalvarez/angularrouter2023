68c22fe33966c204b15940e5a0a5c1c4f187460b
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-10 11:28:46 +0200

    nuevos barra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
 import { C3Component } from './c3/c3.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,9 @@ import { C3Component } from './c3/c3.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
