6b51ab06292ba2cfae30afb7bc9041c042aad63b
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-10 10:56:53 +0200

    ruta1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { C1Component } from './c1/c1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"c1",component:C1Component, pathMatch:"full"}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
