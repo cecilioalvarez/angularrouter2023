815666d54b0f6a072155c2e114bd8194cb149b12
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-10 11:17:57 +0200

    navegacion elemental

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
 
 const routes: Routes = [
 {path:"", redirectTo:"/c1",pathMatch:"full"},
 {path:"c1",component:C1Component, pathMatch:"full"},
 {path:"c2",component:C2Component, pathMatch:"full"},
+{path:"c3",component:C3Component, pathMatch:"full"},
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     C1Component,
-    C2Component
+    C2Component,
+    C3Component
   ],
   imports: [
     BrowserModule,
