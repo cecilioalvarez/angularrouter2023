ae50dd5ebb549acf06bba8abc3ba3af0e7c8091b
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-10 11:09:31 +0200

    router c1 y c2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
 
 const routes: Routes = [
-{path:"c1",component:C1Component, pathMatch:"full"}
+{path:"", redirectTo:"/c1",pathMatch:"full"},
+{path:"c1",component:C1Component, pathMatch:"full"},
+{path:"c2",component:C2Component, pathMatch:"full"},
+
 
 
 ];
